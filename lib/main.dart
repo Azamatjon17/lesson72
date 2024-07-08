33472be8b8f1fe50d4c93cc099b4f6784278ede8
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-07-08 21:54:40 +0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:lesson72/firebase_options.dart';
+import 'package:lesson72/services/firestore_firebase_service.dart';
+import 'package:lesson72/services/location_service.dart';
+import 'package:lesson72/views/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+// ignore: depend_on_referenced_packages
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await LocationService.init();
+
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+
+
+  @override
+  Widget build(BuildContext context) {
+  
+    return ChangeNotifierProvider(
+        create: (context) => FirestoreFirebaseService(),
+        builder: (context, child) {
+          return const MaterialApp(
+            home: HomeScreen(),
+          );
+        });
+  }
+}
